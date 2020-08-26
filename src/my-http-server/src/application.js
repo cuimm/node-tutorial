c87f89a2bdac115e0cf4f76fd828c33478276d4b
mengmeng19 <mengmeng19@staff.weibo.com>
2020-08-26 12:06:01 +0800

    cache && gzip

--- src/my-http-server/src/application.js
+++ src/my-http-server/src/application.js
@@ -1,8 +1,13 @@
+/** 核心模块 **/
 const http = require('http');
-const chalk = require('chalk');
 const url = require('url');
 const path = require('path');
 const fs = require('fs').promises;
+const crypto = require('crypto');
+const zlib = require('zlib');
+
+/** 第三方模块 **/
+const chalk = require('chalk');
 const {createReadStream} = require('fs')
 const mime = require('mime');
 const ejs = require('ejs');
@@ -27,12 +32,11 @@ class Server {
   async handleRequest(req, res) {
     const {pathname} = url.parse(req.url);
     const filePath = path.join(this.directory, pathname);
-    // console.log('filePath', filePath);
     try {
       // 检查文件
       const statObj = await fs.stat(filePath);
       if (statObj.isFile()) {
-        this.handleFile(filePath, res);
+        this.handleFile(filePath, req, res, statObj);
       } else {
         this.handleDirectory(filePath, res);
       }
@@ -48,7 +52,6 @@ class Server {
    * @returns {Promise<void>}
    */
   async handleDirectory(dirPath, res) {
-    console.log('handleDirectory');
     // 读取文件夹
     const dirs = await fs.readdir(dirPath);
     // 读取模版
@@ -65,15 +68,86 @@ class Server {
    * @param res
    * @returns {Promise<void>}
    */
-  async handleFile(filePath, res) {
+  async handleFile(filePath, req, res, statObj) {
     res.setHeader('Content-Type', `${mime.getType(filePath)}; charset=utf-8;`);
-    createReadStream(filePath).pipe(res)
+    const cache = await this.cache(req, res, filePath, statObj);
+    // if (cache) {
+    //   res.statusCode = 304;
+    //   return res.end();
+    // }
+    // 如果没有缓存，就返回压缩文件
+    const gzip = this.gzip(req, res);
+    if (gzip) {
+      // 如果支持压缩，就返回压缩流（test.html文件：gzip压缩前17.9k，gzip压缩后569B）
+      createReadStream(filePath).pipe(gzip).pipe(res);
+    } else {
+      createReadStream(filePath).pipe(res);
+    }
   }
 
   handleError(error, res) {
     console.log('error', error);
     res.statusCode = 404;
     res.end('Not Found');
+  }
+
+  /**
+   * 缓存处理
+   * @param req
+   * @param res
+   * @param filePath
+   * @param statObj
+   * @returns {Promise<boolean>}
+   */
+  async cache(req, res, filePath, statObj) {
+    // 强制缓存-设置响应头 cache-control
+    res.setHeader('Cache-Control', 'max-age=10'); // cache-control 强制缓存 10s
+    // 强制缓存-设置响应头 Expires
+    res.setHeader('Expires', new Date(Date.now() + 10 * 1000).toGMTString()); // expires 强制缓存 10s
+
+    // 协商缓存-设置响应头Etag （Etag / If-none-match）此处将文件内容hash作为etag,粗略计算文件内容
+    const content = await fs.readFile(filePath);
+    const eTag = this.md5(content);
+    res.setHeader('etag', eTag);
+
+    // 协商缓存-设置响应头 （last-modified / if-modified-since）
+    const lastModified = statObj.ctime.toGMTString();
+    res.setHeader('last-modified', lastModified);
+
+    // 协商缓存-对比
+    const ifNoneMatch = req.headers['if-mone-match'];
+    const ifModifiedSince = req.headers['if-modified-since'];
+    // 协商缓存-命中Etag
+    if (ifNoneMatch === eTag) {
+      return true;
+    }
+    // 协商缓存-命中last-modified
+    if (ifModifiedSince === lastModified) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * gzip压缩
+   * @param req
+   * @param res
+   * @returns {*}
+   */
+  gzip(req, res) {
+    const acceptEncoding = req.headers['accept-encoding'];
+    if (acceptEncoding && acceptEncoding.includes('gzip')) {
+      // 返回头中添加 Content-Encoding: gzip：浏览器看到这个标示会自动解压缩
+      res.setHeader('Content-Encoding', 'gzip');
+      // 返回gzip对象
+      return zlib.createGzip();
+    } else {
+      return false;
+    }
+  }
+
+  md5(val) {
+    return crypto.createHash('md5').update(val).digest('base64');
   }
 }
 
