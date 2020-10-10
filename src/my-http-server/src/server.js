b34f11468dca71822a733f3ab6a46eff71672147
mengmeng19 <mengmeng19@staff.weibo.com>
2020-10-10 18:31:40 +0800

    add comment

--- src/my-http-server/src/server.js
+++ src/my-http-server/src/server.js
@@ -1,7 +1,7 @@
 const Server = require('./application');
 
 /**
- * 创建服务（工厂服务）
+ * 创建服务（工厂服务）- 将new操作单独封装起来，不关注内部实现细节；屏蔽内部实现细节，只提供创建的入口。
  * @param port  端口号
  * @param address 地址
  * @param directory 启动目录
